681c3942cb92dd3a8314c1806e1e989e53336263
wcz0 <wcz0@outlook.com>
2022-09-22 17:35:51 +0800

    commit files

---
+++ README.md
@@ -0,0 +1,15 @@
+# GoFrame Documention
+
+GF 文档, 非官方项目
+
+整理gf官方文档
+
+发动社区力量, 欢迎PR
+
+官方地址 [https://goframe.org/display/gf](https://goframe.org/display/gf)
+
+使用[Vitepress](https://vitepress.vuejs.org/ "Viteoress")构建
+
+整理文档不易, 请我喝杯咖啡 [link](https://afdian.net/a/wcz0_)
+
+联系 [wcz0@outlook.com](mailto:wcz0@outlook.com "联系我")
---
+++ docs/.vitepress/config.js
@@ -0,0 +1,69 @@
+
+export default{
+    title: 'GoFrame',
+    description: 'This is a GoFrame Framework Documentation',
+    themeConfig: {
+        nav: [
+            {text: '指南', link: '/index'},
+            {
+                text: '关于',
+                items: [],
+            },
+            {text: '项目地址', link: 'https://github.com/wcz0/gf-docs',}
+        ],
+        sidebar: [
+            {
+                text: '前言',
+                items: [
+                    {text: '项目介绍', link: '/index'},
+                    {text: '核心架构', link: '/what-gf'}
+                ]
+            },
+            {
+                text: '快速入门',
+                items: [
+                    {text: '安装', link: '/install'},
+
+                ]
+            },
+            {
+                text: '基础功能',
+                items: [
+                    {text: '安装', link: '/install'},
+
+                ]
+            },
+            {
+                text: '基础功能',
+                items: [
+                    {text: '安装', link: '/install'},
+
+                ]
+            },
+            {
+                text: '网络服务',
+                items: [
+                    {text: '安装', link: '/install'},
+                    
+                ]
+            },
+            {
+                text: '基础功能',
+                items: [
+                    {text: '安装', link: '/install'},
+
+                ]
+            },
+            {
+                text: '版本管理',
+                items: [
+                    {text: '安装', link: '/install'},
+                    {text: '版本更新说明', link: '/version/v2'},
+                ]
+            }
+        ],
+        socialLinks: [
+            { icon: 'github', link: 'https://github.com/gogf/gf' },
+        ]
+    }
+}
